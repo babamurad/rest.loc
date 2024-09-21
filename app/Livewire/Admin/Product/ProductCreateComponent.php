5c963c62129261d9e4d2e1f98db320915e0dabef
babamurad <babamurad2010@yandex.ru>
2024-09-21 17:58:36 +0500

    Product Create

--- app/Livewire/Admin/Product/ProductCreateComponent.php
+++ app/Livewire/Admin/Product/ProductCreateComponent.php
@@ -2,11 +2,16 @@
 
 namespace App\Livewire\Admin\Product;
 
+use App\Models\Category;
+use Illuminate\Support\Str;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class ProductCreateComponent extends Component
 {
+    use WithFileUploads;
+
     public $name, $slug, $thumb_image, $category_id;
     public $price, $offer_price, $short_description, $long_description;
     public $sku, $status, $is_featured, $show_at_home, $seo_title, $seo_description;
@@ -16,9 +21,27 @@ class ProductCreateComponent extends Component
         'slug' => ['required','string','max:255','unique:categories,slug'],
     ];
 
+    public function cancel()
+    {
+        $this->reset(['name','slug','status','order','show_at_home']);
+        $this->redirect(route('admin.category.index'), navigate:true);
+    }
+
+    public function generateSlug()
+    {
+        $this->slug = Str::slug($this->name);
+    }
+
+    public function generateSKU($category, $model, $color)
+    {
+        return strtoupper(substr($category, 0, 3)) . '-' . $model . '-' . strtoupper(substr($color, 0, 3));
+    }
+
+
     #[Layout('livewire.admin.layouts.admin-app')]
     public function render()
     {
-        return view('livewire.admin.product.product-create-component');
+        $categories = Category::all();
+        return view('livewire.admin.product.product-create-component', compact('categories'));
     }
 }
