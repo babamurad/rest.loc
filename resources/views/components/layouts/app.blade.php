c241cc110a3e701a077b9bb6fb3d2495eca8860c
babamurad <babamurad2010@yandex.ru>
2025-04-07 10:54:34 +0500

    logo

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -141,19 +141,19 @@
                 <div class="col-lg-4 col-sm-8 col-md-6">
                     <div class="fp__footer_content">
                         <a class="footer_logo" href="/">
-                            <img src="{{ asset( $footerInfo->logo ) }}" alt="FoodPark" class="img-fluid w-100">
+                            <img src="{{ asset($footerInfo ? $footerInfo->logo : 'assets/images/logo.png') }}" alt="FoodPark" class="img-fluid w-100">
                         </a>
-                        @if(@$footerInfo->short_info)
+                        @if(isset($footerInfo) && $footerInfo && $footerInfo->short_info)
                         <span>{{ $footerInfo->short_info }}</span>
                         @endif
-                        @if(@$footerInfo->address)
-                        <p class="info"><i class="far fa-map-marker-alt"></i> 7{{ $footerInfo->address }}</p>
+                        @if(isset($footerInfo) && $footerInfo && $footerInfo->address)
+                        <p class="info"><i class="far fa-map-marker-alt"></i> {{ $footerInfo->address }}</p>
                         @endif
-                        @if(@$footerInfo->phone)
-                        <a class="info" href="callto:1234567890123"><i class="fas fa-phone-alt"></i>{{ $footerInfo->phone }}</a>
+                        @if(isset($footerInfo) && $footerInfo && $footerInfo->phone)
+                        <a class="info" href="callto:{{ $footerInfo->phone }}"><i class="fas fa-phone-alt"></i>{{ $footerInfo->phone }}</a>
                         @endif
-                        @if(@$footerInfo->email)
-                        <a class="info" href="mailto:websolutionus1@gmail.com"><i class="fas fa-envelope"></i>{{ $footerInfo->email }}</a>
+                        @if(isset($footerInfo) && $footerInfo && $footerInfo->email)
+                        <a class="info" href="mailto:{{ $footerInfo->email }}"><i class="fas fa-envelope"></i>{{ $footerInfo->email }}</a>
                         @endif
                     </div>
                 </div>
@@ -207,8 +207,10 @@
             <div class="row">
                 <div class="col-12">
                     <div class="fp__footer_bottom_text d-flex flex-wrap justify-content-between">
-                        @if(@$footerInfo->copyright)
+                        @if(isset($footerInfo) && $footerInfo && $footerInfo->copyright)
                         <p>{{ $footerInfo->copyright }}</p>
+                        @else
+                        <p>© 2023 All Rights Reserved</p>
                         @endif
                         <ul class="d-flex flex-wrap">
                             <li><a href="#">FAQs</a></li>
@@ -283,3 +285,5 @@
 </body>
 
 </html>
+
+
